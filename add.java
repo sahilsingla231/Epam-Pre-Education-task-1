fbc5ba76f6f80dc4e2d51d6d08989bef9b10d3db
sahilsingla231 <sahilsingla231231@gmail.com>
2019-03-10 11:17:21 +0530

    Epam Task1

---
+++ add.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class A
+{
+public static void main(String args[])
+{
+int x,y,z;
+Scanner sl=new Scanner(System.in);
+System.out.println("Enter two numbers");
+x=sl.nextInt();
+y=sl.nextInt();
+z=x+y;
+System.out.println("Sum is "+z);
+
+}
+}
